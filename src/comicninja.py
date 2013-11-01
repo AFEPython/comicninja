41b7aa4971f9d8faa3aa3056766269f0d69373df
Mr. Bartosz <toszter@gmail.com>
2013-11-01 11:31:52 -0700

    Changed all tabstops to 4-space indents.

--- src/comicninja.py
+++ src/comicninja.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, views
-from oauth2client.client import credentials_from_code
 
 import os
 import functools
 import urllib,urllib2
-import simplejson
+import json
 from datetime import datetime, timedelta
 
 #configuration
@@ -15,7 +14,6 @@ comicninja.config.from_object(__name__)
 
 ####### SEND THIS TO ITS OWN FILE, EVENTUALLY ########
 # Convenience methods
-
 def login_required(f):
     @functools.wraps(f)
     def wrapper(*args,**kwargs):
@@ -33,58 +31,58 @@ def handle_logout(f):
             session.pop("username",None)
             return redirect(url_for("home"))
         else:
-			return f(*args,**kwargs)
+            return f(*args,**kwargs)
     return wrapper
 
 # The comicninja Object Classification
 class Home(views.MethodView):
-	def get(self):
-		context = {}
-		context["page_title"] = "Welcome to the Comic Ninja Dojo"
-		return render_template("home.html5")
-	@login_required
-	def post(self):
-		pass
+    def get(self):
+        context = {}
+        context["page_title"] = "Welcome to the Comic Ninja Dojo"
+        return render_template("home.html5")
+    @login_required
+    def post(self):
+        pass
 
 class ComicList(views.MethodView):
-	@login_required
-	def get(self):
-		pass
-	@login_required
-	def post(self):
-		pass
+    @login_required
+    def get(self):
+        pass
+    @login_required
+    def post(self):
+        pass
 
 class ComicEdit(views.MethodView):
-	@login_required
-	def get(self):
-		pass
-	@login_required
-	def post(self):
-		pass
+    @login_required
+    def get(self):
+        pass
+    @login_required
+    def post(self):
+        pass
 
 class ComicDelete(views.MethodView):
-	@login_required
-	def delete(self):
-		pass
+    @login_required
+    def delete(self):
+        pass
 
 
 
 ##### SEND THIS CODE TO ITS OWN FILE, EVENTUALLY #####
 # Rules for the comicninja urls, so the comicninjas get to where thy want to go
 comicninja.add_url_rule("/",
-	view_func = Home.as_view('home'),
-	methods = ["GET","POST"])
+    view_func = Home.as_view('home'),
+    methods = ["GET","POST"])
 
 comicninja.add_url_rule("/comics/list",
-	view_func = ComicList.as_view("comic_list"),
-	methods = ["GET","POST"])
+    view_func = ComicList.as_view("comic_list"),
+    methods = ["GET","POST"])
 
 comicninja.add_url_rule("/comics/<comic_id>/edit",
     view_func = ComicEdit.as_view("edit_comic"),
     methods = ["GET","POST"])
 
 comicninja.add_url_rule("/comics/<comic_id>/delete",
-    view_func = ComicDelete.as_view("edit_comic"),
+    view_func = ComicDelete.as_view("delete_comic"),
     methods = ["DELETE"])
 
 if (__name__ == "__main__"):
